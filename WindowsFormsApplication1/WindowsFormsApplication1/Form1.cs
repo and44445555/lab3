1d7e37dd04dd440abf207e5290f4283fd369afdb
and44445555 <9d994@list.ru>
2016-10-27 14:56:31 +0400

    Версия 2

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,9 +12,38 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        
+
+string s;
+double a;
+double b;
+double c;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            s = textBox1.Text;
+            a = Convert.ToDouble(s);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            s = textBox2.Text;
+            b = Convert.ToDouble(s);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c = a + b;
+            textBox3.Text = Convert.ToString(c);
         }
     }
 }
